05998bf7d7d26da48990aeab6cd91920d68f6ec5
Vincent Pretre <v.pretre@zestsoftware.nl>
2012-10-29 17:54:32 +0100

    Resolution and quality of the PDF file can now be set in the ZMI

--- collective/pdftransform/pdf_image.py
+++ collective/pdftransform/pdf_image.py
@@ -13,17 +13,52 @@ class PdfToImage:
 
     def __init__(self,
                  name=None,
-                 inputs=('application/pdf',),
-                 tab_width = 4):
-        self.config = { 'inputs' : inputs, 'tab_width' : 4}
+                 inputs=None,
+                 tab_width=None,
+                 resolution=None,
+                 quality=None):
+
+        if inputs is None:
+            inputs = ('application/pdf',)
+
+        if tab_width is None:
+            tab_width = 4
+
+        if resolution is None:
+            resolution = DEFAULT_OPTIONS['resolution']
+
+        if quality is None:
+            quality = DEFAULT_OPTIONS['quality']
+
+        self.config = {'inputs' : inputs,
+                       'tab_width' : 4,
+                       'resolution': resolution,
+                       'quality': quality}
+
         self.config_metadata = {
             'inputs' : ('list', 'Inputs',
-                            'Input(s) MIME type. Change with care.'),
+                        'Input(s) MIME type. Change with care.'),
             'tab_width' : ('string', 'Tab width',
-                            'Number of spaces for a tab in the input')
+                           'Number of spaces for a tab in the input'),
+            'resolution' : ('string', 'Resolution',
+                            'Default resolution'),
+            'quality': ('string', 'Quality',
+                        'PDF quality')
             }
         if name:
             self.__name__ = name
+
+    def get_options(self):
+        """ Uses the default options from pdfpeek.
+        Checks in portal_properties if any is overriden.
+        """
+        options = dict([(k, v) for k, v in DEFAULT_OPTIONS.items()])
+        for key in options:
+            p_val = self.config.get(key, None)
+            if p_val is not None:
+                options[key] = p_val
+
+        return options
 
     def name(self):
         return self.__name__
@@ -35,8 +70,9 @@ class PdfToImage:
 
     def convert(self, orig, data, **kwargs):
         converter = convertPDFToImage()
-        options = DEFAULT_OPTIONS
-        if 'pdf_resolution' in kwargs:
+        options = self.get_options()
+
+        if kwargs.get('pdf_resolution', None) is not None:
             options['resolution'] = kwargs['pdf_resolution']
 
         img = converter.ghostscript_transform(orig, 1, options)
